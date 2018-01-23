cda68d77e27c0ef2d4a11d6a985989f3f9141560
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-01-23 00:22:23 -0800

    try

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,109 @@
+
 window.onload = function() {
 
+    // document.onkeyup = function(event) {
+    //   var userGuess = event.key;
+
+ 
+// Create an array of elements
 var elements = ["hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen","oxygen","fluorine","neon","sodium","magnesium","aluminum","silicon","phosphorus","sulfur","chlorine","argon","potassium","calcium"];
-//generates random #, and assigns it to an item in the array, storing it in random
+// Pick a random element from the list
 var random = elements[Math.floor((Math.random() * elements.length))];
 // Initiating variables
 var win =0;
 var lose =0;
-var letterGuess = [];
-var indexes = [];
+var count = 7;
+
 var isMatch = false;
 
 
-//call underscore function to display random underline
-underscore();
+// Split the word into charaters
+var char = random.split("");
+
+// Display underlines of the randomly pick element
+var underlines = [];
+
+ for (var i = 0; i < char.length; i++) {
+     underlines[i] = [ "_ "] ;  
+ }
+ var gameDiv = document.getElementById("game");
+ var displayDiv = document.getElementById("display");
+ var guessedDiv = document.getElementById("guessed");
+ var remainDiv = document.getElementById("remain");
 
 
+
+      displayDiv.innerHTML = underlines.join(" ");
+
+      
+ // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
+      // Determines which key was pressed.
       var userGuess = event.key;
-
-    }
-
-    for (var j = 0; j < random.length; j++) {
-        
-        if (random[j] === letterGuess[letterGuess.length-1]) {
-            isMatch = true;
-            var splitWord = underline.split("");
-            for (k = 0; k < indexes.length; k++) {
-                splitWord[indexes[k]] = letterGuess[letterGuess.length-1];
-                indexes = [];
-                                underline = splitWord.join("");
+ 
+ // This for loop matches the user input with according char and return an array with 1 and 0
+       var index = [];
+        for (var k = 0; k < char.length; k++){
+            if (userGuess == char[k]){
+                 index[k] = 1;
             }
-        } 
-    }
-
-    if (isMatch) {
-        document.getElementById('random').innerHTML = underline;
-        letterGuess.pop();
-
-    } else {
-  
-        guessesRemaining--
-        document.getElementById('guessed').innerHTML = letterGuess;
-        //display remaining chances
-        document.getElementById('remainingGuesses').innerHTML = "Guesses Remaining: " + guessesRemaining;
-    }
-
-    isMatch = false;
-    checkWin();
-    images();
-    
-});
-
-
-
-function underscore() {
-    for (var i = 0; i < random.length; i++) {
-        underline += "_";
-    }
-    document.getElementById('random').innerHTML = underline;
-}
-
-function checkWin() {
-    if (underline === random) {
-            alert("You Win!");
-        } else if (guessesRemaining === 0) {
-            alert("You Lose! Retake CHEM 1A.");
-            document.getElementById('random').innerHTML = random;
+            else {
+                index[k] = 0;
+            }
         }
-}   
+  // This for loop replace the underlines with matching chars, resulting from the above index array
+        for (var l = 0; l < char.length; l++) {
+            if (index[l] == 1) {
+            underlines[l] = userGuess;
+            }
+        }
 
-function images(){
-    if (guessesRemaining === 6) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/1.png height='400'/>";
 
-    } else if (guessesRemaining === 5) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/2.png height='400'/>";
 
-    } else if (guessesRemaining === 4) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/3.png height='400'/>";
 
-    } else if (guessesRemaining === 3) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/4.png height='400'/>";
-        
-    } else if (guessesRemaining === 2) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/5.png height='400'/>";
-        
-    } else if (guessesRemaining === 1) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/6.png height='400'/>";
 
-    } else if (guessesRemaining === 0) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/7.png height='400'/>";   
-    }
+
+var sum = index.reduce(function(a, b) { return a + b; }, 0);
+var letterGuess = [];
+if (sum == 0){
+    letterGuess = letterGuess.push(userGuess);
+}
+   var newguessDiv = document.createElement("div");
+      newguessDiv.innerHTML = letterGuess;
+
+         guessedDiv.appendChild(newguessDiv);
+
+
+ // For checking purpose
+    console.log(index);
+    console.log(char);
+    console.log(random);
+    console.log(userGuess);
+    console.log(underlines);
+    console.log(letterGuess);
+
+
+// Update a new display
+
+displayDiv.innerHTML = underlines.join(" ");
+guessedDiv.innerHTML = letterGuess;
+guessedDiv.innerHTML = count;
+
+
+gameDiv.appendChild(displayDiv);
+// gameDiv.appendChild(guessedDiv);
+
+
+
+
+
+
 }
 
 
 }
+
+    //   displayDiv.appendChild(newDiv);
+
+
 
